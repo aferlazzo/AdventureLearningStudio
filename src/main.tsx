1ada5473d6be562af154c48744dccba069c84f5b
Anthony Ferlazzo <aferlazzo@gmail.com>
2026-07-21 17:46:56 -0700

    Load studio home styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/global.css";
 import "./styles/comic-builder.css";
 import "./styles/image-upload.css";
+import "./styles/studio-home.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
