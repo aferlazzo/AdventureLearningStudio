29dcd41395fba754fea7a5678dc237d3a622d5ff
Anthony Ferlazzo <aferlazzo@gmail.com>
2026-07-21 17:23:54 -0700

    Load comic image upload styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/comic-builder.css";
+import "./styles/image-upload.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
