90c2c2fb09a202b31c958193e299a7c384983d42
Anthony Ferlazzo <aferlazzo@gmail.com>
2026-07-21 17:56:47 -0700

    Load guided workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import "./styles/global.css";
 import "./styles/comic-builder.css";
 import "./styles/image-upload.css";
 import "./styles/studio-home.css";
-import "./styles/workspace-experience.css";
+import "./styles/workspace-polish.css";
+import "./styles/guided-workspace.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
